3b13ae116be21832b390a74fbeec08c98ee0c729
HNZencir <hnz.2008.izmir@gmail.com>
2021-03-28 15:37:51 +0300

    Commit message

---
+++ 5.py
@@ -0,0 +1,14 @@
+sorular = ["Bir yılda kaç ay var?(cevap 12)","Güneş sisteminde kaç gezegen vardır?(cevap 8)","10/2 kaç eder? (cevap 5)"]
+cevaplar = [12,8,5]
+soru_no = 0
+puan = 0
+while soru_no < len(sorular):
+    print(sorular[soru_no])
+    cevap = int(input("Cevap:\n"))
+    if cevap == cevaplar[soru_no]:
+        puan += 10
+        print("Doğru cevap! Puanın: {}".format(puan))
+    else:
+        puan -= 10
+        print("Yanlış cevap :( Puanın: {}".format(puan))
+    soru_no += 1
